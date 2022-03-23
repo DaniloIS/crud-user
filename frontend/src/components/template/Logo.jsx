128279ed8f872171523cc988d6787ff82067a4b6
DaniloIS <dailosantos-pg@hotmail.com>
2022-03-23 14:54:54 -0300

    Rotas e componente UserCrud

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/img/logo.png';
 import './Logo.css';
 
 export default props => 
     <aside className='logo'>
-        <a href='/' className='logo'>
+        <Link to='/' className='logo'>
             <img src={logo} alt='logo' />
-        </a>
+        </Link>
     </aside>
